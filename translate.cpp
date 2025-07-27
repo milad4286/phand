28f6f3245c3f4b6882b199fbe99717f716e84bec
milad <miladmohammadigit@gmail.com>
2025-07-27 19:32:58 +0330

    second test

--- translate.cpp
+++ translate.cpp
@@ -1,23 +1,41 @@
-#include "translate.h".h"
-#include<QWidget>
-#include <QVBoxLayout>
+#include "translate.h"
+#include <QFileDialog>
+#include <QPixmap>
 
-translate::translate(QWidget *parent) : QWidget(parent) {
+translate::translate(QWidget *parent)
+    : QWidget(parent)
+{
+    imageLabel = new QLabel(this);
+    imageLabel->setAlignment(Qt::AlignCenter); // وسط چین کردن لیبل
+    imageLabel->setText("هیچ تصویری بارگذاری نشده"); // متن پیش فرض برای کاربر
 
-    auto *centralwig = new QWidget(this) ;
+    loadButton = new QPushButton("انتخاب تصویر", this);
+    connect(loadButton, &QPushButton::clicked, this, &translate::loadImage);
 
-    auto *vmainlayout = new QVBoxLayout() ;
-    vmainlayout->addWidget(centralwig);
-    this->setLayout(vmainlayout);
+    QVBoxLayout *layout = new QVBoxLayout(this);
+    layout->addWidget(imageLabel);
+    layout->addWidget(loadButton);
+    setLayout(layout);
+}
 
+// privat slots
+void translate::loadImage()
+{
+    QString filePath = QFileDialog::getOpenFileName(this, "انتخاب تصویر", "", "Images (*.png *.jpg *.bmp)");
 
+    if (!filePath.isEmpty()) {
+        QImage image(filePath);
+        if (!image.isNull()) {
+            loadedImage = image; // ذخیره برای پردازش بعدی
+            displayImage(loadedImage);
+        } else {
+            imageLabel->setText("خطا در بارگذاری تصویر!");
+        }
+    }
+}
 
-
-
-
-
-
-
-
-
+void translate::displayImage(const QImage &image)
+{
+    QPixmap pixmap = QPixmap::fromImage(image);
+    imageLabel->setPixmap(pixmap.scaled(400, 400, Qt::KeepAspectRatio, Qt::SmoothTransformation));
 }
--- translate.h
+++ translate.h
@@ -1,14 +1,27 @@
-#ifndef TRANSLATEPAGE_H
-#define TRANSLATEPAGE_H
+#ifndef TRANSLATE_H
+#define TRANSLATE_H
 
 #include <QWidget>
+#include <QLabel>
+#include <QPushButton>
+#include <QVBoxLayout>
+#include <QImage> // مناسب هم برای دخیره و هم برای پردازش تصویر بر خلاف pixmap
+
 class translate : public QWidget
 {
     Q_OBJECT
-signals:
 
 public:
-    translate(QWidget *parent = nullptr);
+    explicit translate(QWidget *parent = nullptr);
+
+private slots:
+    void loadImage(); // بارگذاری عکس از کاربر
+
+private:
+    QLabel *imageLabel; //  لیبل برای نمایش عکس
+    QImage loadedImage; // تصویر اصلی برای پردازش
+    QPushButton *loadButton;
+    void displayImage(const QImage &image); // تابع برای نمایش عکس
 };
 
-#endif // TRANSLATEPAGE_H
+#endif // TRANSLATE_H
